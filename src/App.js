c4fe51396d6a93b7157d310f7ffea1251e77f08d
tomlightning <le.tomer@gmail.com>
2023-01-01 15:11:35 +0200

    initial project for neiman

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Profile from "./components/profile";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { EthereumClient, modalConnectors } from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { mainnet, goerli } from "wagmi/chains";
+
+import "./assets/css/index.css";
+import App from "./App";
+
+const chains = [mainnet, goerli];
+
+// Wagmi client
+const { provider, webSocketProvider } = configureChains(chains, [
+  publicProvider(),
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "Woolball Woolballers", chains }),
+  provider,
+  webSocketProvider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <WagmiConfig client={wagmiClient}>
+      <App />
+    </WagmiConfig>
+    <Web3Modal ethereumClient={ethereumClient} />
+  </React.StrictMode>
+);
