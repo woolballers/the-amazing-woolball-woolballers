8f1fd0f6f72ba587d633097acaf848e7ce18a62f
neiman <neiman@neiman.co.il>
2023-01-05 13:33:13 +0100

    add WoolName component for writing Woolball names (like Neiman#), add a register button to navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,43 +1,36 @@
 import Profile from "components/profile";
+import WoolName from "components/woolName"
+
 
 function Navbar() {
-  return (
+  return(
     <div className="border-bottom border-danger mb-4">
       <nav className="navbar navbar-expand-lg nav-fill">
-        <a className="navbar-brand" href="#">
-          Woolballers
-        </a>
+        <a className="navbar-brand" href="#">Woolballers</a>
         <form className="d-flex">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search Name"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-success"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#searchResults"
-            aria-expanded="false"
-            aria-controls="searchResults"
-          >
-            <i className="bi bi-search"></i>
+          <input className="form-control me-2" type="search" placeholder="Search Name" aria-label="Search"/>
+          <button className="btn btn-outline-success" type="button" data-bs-toggle="collapse" data-bs-target="#searchResults" aria-expanded="false" aria-controls="searchResults">
+            <i class="bi bi-search"></i>
           </button>
+
+
         </form>
 
-        <Profile />
+        < Profile />
       </nav>
 
-      <nav className="navbar">
-        <div className="collapse multi-collapse w-100" id="searchResults">
-          <div className="card card-body">
-            Some placeholder content for the search results of the name.
+      <nav class="navbar">
+        <div class="collapse multi-collapse w-100" id="searchResults">
+          <div class="card card-body flex-row">
+            <WoolName name="Neiman"/>&nbsp;belongs to 0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5.
+          </div>
+          <div class="card card-body flex-row">
+            <WoolName name="Neiman"/>&nbsp;is unregistered <button className="btn btn-warning">Register</button>
           </div>
         </div>
       </nav>
     </div>
-  );
+  )
 }
 
 export default Navbar;
---
+++ src/components/woolName/index.js
@@ -0,0 +1,10 @@
+function WoolName(props) {
+  return (
+    <span className="GrislyBeast">
+      {props.name}
+      <span style={{color:'orange'}}>#</span>
+    </span>
+  )
+}
+
+export default WoolName;
