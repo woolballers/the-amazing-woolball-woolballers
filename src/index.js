6a44e02cda39742f4fffd1019423b134db88d7aa
tomlightning <le.tomer@gmail.com>
2023-01-05 14:25:46 +0200

    fix bootstrap-icons

--- src/index.js
+++ src/index.js
@@ -4,18 +4,19 @@ import { EthereumClient, modalConnectors } from "@web3modal/ethereum";
 import { Web3Modal } from "@web3modal/react";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
-import { mainnet, goerli } from "wagmi/chains";
+import { mainnet, goerli, hardhat } from "wagmi/chains";
 
 import App from "./App";
 
 import "./assets/css/index.css";
 import "./assets/css/augmented-ui.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 // bootstrap JS, consider changing to import only the functionality we use
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-const chains = [mainnet, goerli];
+const chains = [mainnet, goerli, hardhat];
 
 // Wagmi client
 const { provider, webSocketProvider } = configureChains(chains, [
