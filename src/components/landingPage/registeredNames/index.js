a8aa772f37eb74a326a30986ebc36da8623c8304
tomlightning <le.tomer@gmail.com>
2023-01-09 22:07:02 +0200

    started working on all names box

--- src/components/landingPage/registeredNames/index.js
+++ src/components/landingPage/registeredNames/index.js
@@ -1,7 +1,61 @@
-import WoolName from "components/woolName";
 import SingleRegisteredName from "./SingleRegisteredName";
+import { useContractRead, useContractReads } from "wagmi";
+import {
+  WOOLBALL_CONTRACT_ADDRESS,
+  WOOLBALL_CONTRACT_CHAIN_ID,
+} from "constants/contract";
+import woolballABI from "contracts/woolballABI.json";
+import { useState } from "react";
+
+const woolballContract = {
+  address: WOOLBALL_CONTRACT_ADDRESS,
+  abi: woolballABI,
+  chainId: WOOLBALL_CONTRACT_CHAIN_ID,
+};
 
 export default function RegisteredNames({ setSelectedName, selectedName }) {
+  const [namesLength, setNamesLength] = useState(0);
+  const [isLoadingLen, setIsLoadingLen] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const contractReadNamesLength = useContractRead({
+    ...woolballContract,
+    functionName: "getNamesLen",
+
+    onSettled() {
+      setIsLoadingLen(false);
+    },
+    onSuccess(data) {
+      console.log("contractReadNamesLength", data);
+      setNamesLength(data.toNumber());
+    },
+  });
+
+  console.log(
+    "array of indexes",
+    [...Array(namesLength).keys()].map((val) => {
+      return { ...woolballContract, functionName: "arrNames", args: [val] };
+    })
+  );
+
+  console.log("enabled", isLoadingLen === false && namesLength > 0);
+  const contractReadNames = useContractReads({
+    contracts:
+      (isLoadingLen === false &&
+        namesLength > 0 &&
+        [...Array(namesLength).keys()].map((val) => {
+          return { ...woolballContract, functionName: "arrNames", args: [val] };
+        })) ||
+      [],
+    enabled: isLoadingLen === false && namesLength > 0,
+    onSettled() {
+      setIsLoading(false);
+    },
+    onSuccess(data) {
+      console.log("contractReadNames", data);
+    },
+  });
+
   const nameStyle = (name) => {
     if (name !== selectedName) return "btn bg-transparent";
     else return "btn bg-transparent border border-4 border-primary";
